da94fadaa0e8977e56bd62982168f195d7737075
Karen Grigoryan <karen@karenjs.com>
2024-02-18 12:43:03 +0100

    feat: add duti, chatblade and brew wrap func

--- home/dot_config/private_fish/config.fish
+++ home/dot_config/private_fish/config.fish
@@ -42,6 +42,9 @@ set -gx GOPATH $HOME/go
 # OpenAI
 set -gx OPENAI_API_KEY (pass show openai/api/token)
 
+# Chatblade
+set -gx OPENAI_API_MODEL 4t
+
 # Docker
 set -gx DOCKER_HIDE_LEGACY_COMMANDS true
 
@@ -97,6 +100,7 @@ alias fzfi='git ls-files --cached --others --exclude-standard 2>/dev/null || fd 
 alias ghs='gh copilot suggest'
 alias ghx='gh copilot explain'
 alias nvm='fnm'
+alias appids='lsappinfo | grep 'bundleID="' | cut -d'"' -f2 | sort'
 
 if hash lsd 2>/dev/null
     alias ls='lsd -A'
@@ -108,4 +112,14 @@ else
     alias l='ls -lAh'
     alias ll='ls -lAtrh'
     alias la='ls -A'
+end
+
+# function that wraps brew
+# and adds brew bundle dump --file=~/.local/share/chezmoi/home/Brewfile
+# after brew update, upgrade, install, uninstall commands
+function brew --wraps brew
+    command brew $argv
+    if contains -- update $argv || contains -- upgrade $argv || contains -- install $argv || contains -- uninstall $argv
+        command brew bundle dump --file=~/.local/share/chezmoi/home/Brewfile --no-lock --force
+    end
 end
