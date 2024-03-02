0993c0d2c6154a3e7fd2d40b80b1c9489bcbe9c1
Karen Grigoryan <karen@karenjs.com>
2024-03-03 00:35:30 +0100

    refactor: improve AI plugin command execution in Neovim
    - Encapsulate command execution in a global function table
    - Enhance prompt message with conventional commit example
    - Bind new function to <leader>aid keymap with non-recursive mapping

--- home/dot_config/exact_nvim/exact_plugin/ai.lua
+++ home/dot_config/exact_nvim/exact_plugin/ai.lua
@@ -1,7 +1,22 @@
-vim.api.nvim_set_keymap("n", "<leader>aid", ":lua execute_my_command()<CR>", { noremap = true })
+_G.myfuncs = _G.myfuncs or {}
+_G.myfuncs.execute_my_command = function()
+  local prompt = [[
+  Summarize git diff output given above as a commit. 
+  Each line should be max 70 characters long. 
 
-function execute_my_command()
-  vim.cmd(
-    ':r! git diff --cached | chatblade -e "Summarize above given git diff as a commit. Each line should be max 70 characters long. Give me only the commit text."'
-  )
+  Give me only the commit text.
+  Format commit message using conventional commits. 
+
+  Example:
+
+  feat: add new feature
+
+  - detail one
+  - detail two
+  - detail three
+  ...
+  ]]
+  vim.cmd(":r! git diff --cached | chatblade -e '" .. vim.fn.shellescape(prompt) .. "'")
 end
+
+vim.api.nvim_set_keymap("n", "<leader>aid", ":lua _G.myfuncs.execute_my_command()<CR>", { noremap = true })
