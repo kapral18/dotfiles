22265f0a0fdb54aa56e183d3ebcbc973a64cd340
Karen Grigoryan <karen@karenjs.com>
2024-04-27 22:58:02 +0200

    refactor(nvim): remove treesitter-refactor and ts-swap plugins
    - Remove treesitter-refactor plugin configuration
    - Simplify treesitter-textobjects plugin setup
    - Replace ts-swap with sibling-swap plugin
    - Update tmux configuration for terminal compatibility
    - Change thumbs key binding in tmux from 'y' to 'C-t'

--- home/dot_config/exact_nvim/exact_lua/exact_plugins/treesitter-textobjects.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/treesitter-textobjects.lua
@@ -4,136 +4,29 @@ return {
     enabled = false,
   },
   {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    lazy = true,
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      opts = function(_, opts)
-        opts.highlight = {
-          enable = true,
-          use_languagetree = true,
-        }
-        opts.indent = { enable = true }
-        opts.playground = { enable = true }
-        opts.autotag = { enable = true }
-        opts.incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "<Enter>",
-            node_incremental = "<Enter>",
-            node_decremental = "<BS>",
-          },
-        }
-        opts.textobjects = { --{{{
-          select = { -- {{{
-            enable = true,
-            keymaps = {
-              ["aC"] = "@class.outer",
-              ["aa"] = "@parameter.outer",
-              ["ab"] = "@block.outer",
-              ["ac"] = "@conditional.outer",
-              ["ad"] = "@comment.outer",
-              ["ae"] = "@block.outer",
-              ["af"] = "@function.outer",
-              ["ak"] = "@assignment.lhs",
-              ["al"] = "@loop.outer",
-              ["am"] = "@call.outer",
-              ["as"] = "@statement.outer",
-              ["au"] = "@call.outer",
-              ["av"] = "@assignment.rhs",
-              ["iC"] = "@class.inner",
-              ["ia"] = "@parameter.inner",
-              ["ib"] = "@block.inner",
-              ["ic"] = "@conditional.inner",
-              ["ie"] = "@block.inner",
-              ["if"] = "@function.inner",
-              ["ik"] = "@assignment.lhs",
-              ["il"] = "@loop.inner",
-              ["im"] = "@call.inner",
-              ["is"] = "@statement.inner",
-              ["iu"] = "@call.inner",
-              ["iv"] = "@assignment.rhs",
-            },
-          }, --}}}
-
-          move = { --{{{
-            enable = true,
-            set_jumps = true,
-            goto_next_start = {
-              ["]f"] = { query = "@function.outer", desc = "Go to start of the next function" },
-              ["]b"] = { query = "@block.outer", desc = "Go to start of the next block" },
-              ["]gc"] = { query = "@comment.outer", desc = "Go to start of the next comment" },
-              ["]a"] = { query = "@parameter.inner", desc = "Go to start of the next parameter" },
-              ["]o"] = { query = "@loop.*", desc = "Go to the next loop" },
-              ["]s"] = {
-                query = "@scope",
-                query_group = "locals",
-                desc = "Go to the next scope",
-              },
-            },
-            goto_next_end = {
-              ["]F"] = { query = "@function.outer", desc = "Go to end of the next function" },
-              ["]B"] = { query = "@block.outer", desc = "Go to end of the next block" },
-              ["]A"] = { query = "@parameter.outer", desc = "Go to end of the next parameter" },
-            },
-            goto_previous_start = {
-              ["[f"] = { query = "@function.outer", desc = "Go to start of the previous function" },
-              ["[b"] = { query = "@block.outer", desc = "Go to start of the previous block" },
-              ["[gc"] = { query = "@comment.outer", desc = "Go to start of the previous comment" },
-              ["[a"] = { query = "@parameter.inner", desc = "Go to start of the previous parameter" },
-              ["[o"] = { query = "@loop.*", desc = "Go to the previous loop" },
-              ["[s"] = {
-                query = "@scope",
-                query_group = "locals",
-                desc = "Go to the previous scope",
-              },
-            },
-            goto_previous_end = {
-              ["[F"] = { query = "@function.outer", desc = "Go to end of the previous function" },
-              ["[B"] = { query = "@block.outer", desc = "Go to end of the previous block" },
-              ["[A"] = { query = "@parameter.outer", desc = "Go to end of the previous parameter" },
-            },
-          }, --}}}
-
-          swap = { --{{{
-            enable = true,
-            swap_next = {
-              ["<leader>.f"] = {
-                query = "@function.outer",
-                desc = "Swap around with the next function",
-              },
-              ["<leader>.e"] = { query = "@element", desc = "Swap with the next element" },
-              ["<leader>.a"] = { query = "@parameter.inner", desc = "Swap with the next parameter" },
-            },
-            swap_previous = {
-              ["<leader>,f"] = {
-                query = "@function.outer",
-                desc = "Swap around with the previous function",
-              },
-              ["<leader>,e"] = { query = "@element", desc = "Swap with the previous element" },
-              ["<leader>,a"] = {
-                query = "@parameter.inner",
-                desc = "Swap with the previous parameter",
-              },
-            },
-          }, --}}}
-
-          lsp_interop = { --{{{
-            enable = true,
-            peek_definition_code = {
-              ["<leader>df"] = { query = "@function.outer", desc = "Peek function definition" },
-            },
-          }, --}}}
-        } -- }}}
-      end,
+    "Wansmer/sibling-swap.nvim",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    opts = {
+      use_default_keymaps = false,
+      highlight_node_at_cursor = false,
     },
-  },
-  {
-    "chrisgrieser/nvim-various-textobjs",
-    opts = { useDefaultKeymaps = true },
-    config = function(_, opts)
-      require("various-textobjs").setup(opts)
-    end,
-    event = "VeryLazy",
+    keys = {
+      {
+        "<leader>.",
+        function()
+          -- swap with right and change operator in between
+          require("sibling-swap").swap_with_right_with_opp()
+        end,
+        desc = "Move Node Right With Opp",
+      },
+      {
+        "<leader>,",
+        function()
+          -- swap with right and change operator in between
+          require("sibling-swap").swap_with_left_with_opp()
+        end,
+        desc = "Move Node Left With Opp",
+      },
+    },
   },
 }
