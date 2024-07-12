e4f60b5c3d99d723677a29524f93d5cdbe3228eb
Karen Grigoryan <karen@karenjs.com>
2024-07-12 23:59:34 +0200

    feat(nvim): enhance nvim-cmp with additional sources and custom window settings
    - Added `cmp-nvim-lsp-signature-help`, `cmp-cmdline`, and `cmp-dap` as dependencies.
    - Configured custom window settings for completion and documentation with rounded borders and specific highlights.
    - Set up `nvim-cmp` for various filetypes including `gitcommit`, `dap-repl`, and `sql`.
    - Configured `nvim-cmp` for command line mode with `/` and `:` mappings.
    - Removed disabled Copilot configurations.

--- home/dot_config/exact_nvim/exact_lua/exact_plugins/ai.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/ai.lua
@@ -1,5 +1,4 @@
 return {
-  { import = "lazyvim.plugins.extras.coding.copilot", enabled = false },
   { import = "lazyvim.plugins.extras.coding.copilot-chat" },
   {
     "dustinblackman/oatmeal.nvim",
@@ -17,48 +16,6 @@ return {
       require("which-key").add({
         { "<leader>om", "<cmd>Oatmeal<CR>", desc = "[AI] Oatmeal: toggle", mode = { "n", "x" } },
       })
-    end,
-  },
-  {
-    "zbirenbaum/copilot.lua",
-    event = "InsertEnter",
-    enabled = false,
-    opts = function()
-      vim.api.nvim_set_hl(0, "CopilotSuggestion", { fg = "#83a598" })
-      vim.api.nvim_set_hl(0, "CopilotAnnotation", { fg = "#03a598" })
-      return {
-        filetypes = {
-          ["*"] = function()
-            local file_size = vim.fn.getfsize(vim.fn.expand("%"))
-            if file_size > 100000 or file_size == -2 then
-              return false
-            end
-            return true
-          end,
-          gitcommit = false,
-          TelescopePrompt = false,
-        },
-        suggestion = {
-          enabled = false,
-          auto_trigger = false,
-          keymap = {
-            accept_word = false,
-            accept_line = false,
-          },
-        },
-        panel = {
-          enabled = false,
-        },
-        server_opts_overrides = {
-          trace = "verbose",
-          settings = {
-            advanced = {
-              listCount = 3,
-              inlineSuggestCount = 3,
-            },
-          },
-        },
-      }
     end,
   },
   {
--- home/dot_config/exact_nvim/exact_lua/exact_plugins/cmp.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/cmp.lua
@@ -1,7 +1,16 @@
+local win_highlight = "Normal:Normal,FloatBorder:Normal,CursorLine:Visual,Search:None"
 return {
   {
     "hrsh7th/nvim-cmp",
     dependencies = {
+      { "hrsh7th/cmp-nvim-lsp-signature-help" },
+      { "hrsh7th/cmp-cmdline" },
+      {
+        "rcarriga/cmp-dap",
+        dependencies = {
+          { "mfussenegger/nvim-dap" },
+        },
+      },
       { "lukas-reineke/cmp-rg" },
       { "lukas-reineke/cmp-under-comparator" },
       { "chrisgrieser/cmp-nerdfont" },
@@ -12,6 +21,8 @@ return {
     },
     opts = function(_, opts)
       local cmp = require("cmp")
+      table.insert(opts.sources, { name = "nvim_lsp_signature_help" })
+      table.insert(opts.sources, { name = "rg" })
       table.insert(opts.sources, { name = "rg", keyword_length = 3 })
       table.insert(opts.sorting.comparators, 4, require("cmp-under-comparator").under)
       table.insert(opts.sources, { name = "dotenv" })
@@ -19,11 +30,51 @@ return {
       table.insert(opts.sources, { name = "fonts", option = { space_filter = "-" } })
       table.insert(opts.sources, { name = "nerdfont" })
 
+      opts.window = {
+        completion = {
+          border = "rounded",
+          winhighlight = win_highlight,
+        },
+        documentation = {
+          border = "rounded",
+          winhighlight = win_highlight,
+        },
+      }
+
       cmp.setup.filetype("gitcommit", {
         sources = cmp.config.sources({
           { name = "git" }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
         }, {
           { name = "buffer" },
+        }),
+      })
+
+      cmp.setup.filetype({ "dap-repl" }, {
+        sources = {
+          { name = "dap" },
+        },
+      })
+
+      cmp.setup.filetype({ "sql" }, {
+        sources = {
+          { name = "vim-dadbod-completion" },
+          { name = "buffer" },
+        },
+      })
+
+      cmp.setup.cmdline("/", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" },
+        },
+      })
+
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          { name = "cmdline" },
         }),
       })
     end,
