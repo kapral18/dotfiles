6997ea373c3bdabcdcc642ae57298038d1f3a2b9
Karen Grigoryan <karen@karenjs.com>
2024-09-12 08:11:57 +0200

    feat(search-and-replace): integrate ssr.nvim for enhanced search and replace capabilities

--- home/dot_config/exact_nvim/exact_lua/exact_plugins/search-and-replace.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/search-and-replace.lua
@@ -54,6 +54,34 @@ return {
     end,
   },
   {
+    "cshuaimin/ssr.nvim",
+    keys = {
+      {
+        "<leader>sx",
+        function()
+          require("ssr").open()
+        end,
+        desc = "Search & Replace",
+        mode = { "n", "x" },
+      },
+    },
+    opts = {
+      border = "rounded",
+      min_width = 50,
+      min_height = 5,
+      max_width = 120,
+      max_height = 25,
+      adjust_window = true,
+      keymaps = {
+        close = "q",
+        next_match = "n",
+        prev_match = "N",
+        replace_confirm = "<cr>",
+        replace_all = "<leader><cr>",
+      },
+    },
+  },
+  {
     "MagicDuck/grug-far.nvim",
     event = "VeryLazy",
     opts = function()
