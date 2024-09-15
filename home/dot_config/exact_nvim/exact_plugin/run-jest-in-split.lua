4e335cb5d1273d1ca217c0d452ca09655bcab9f1
Karen Grigoryan <karen@karenjs.com>
2024-09-15 13:45:06 +0200

    feat: add functionality to close terminal buffers with 'q' key

--- home/dot_config/exact_nvim/exact_plugin/run-jest-in-split.lua
+++ home/dot_config/exact_nvim/exact_plugin/run-jest-in-split.lua
@@ -22,3 +22,26 @@ local function run_current_test()
 end
 
 vim.keymap.set("n", "<Leader>tx", run_current_test, { noremap = true, silent = true })
+
+local function close_terminal_buffer()
+  local buf = vim.api.nvim_get_current_buf()
+
+  if vim.bo[buf].buftype ~= "terminal" then
+    print("This is not a terminal buffer.")
+    return
+  end
+
+  local chan = vim.bo[buf].channel
+
+  vim.fn.jobstop(chan)
+
+  vim.cmd("bdelete! " .. buf)
+end
+
+-- Set up the keymap only for terminal buffers
+vim.api.nvim_create_autocmd("TermOpen", {
+  pattern = "*",
+  callback = function()
+    vim.keymap.set("n", "q", close_terminal_buffer, { noremap = true, silent = true, buffer = true })
+  end,
+})
