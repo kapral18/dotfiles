99f84860ec36cd9f47d892b16dbb94a902499c29
Karen Grigoryan <karen@karenjs.com>
2024-11-15 01:09:59 +0100

    chore(exact_plugins): update astro formatter configuration and remove mdx plugin

--- home/dot_config/exact_nvim/exact_lua/exact_plugins/astro.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/astro.lua
@@ -25,10 +25,8 @@ return {
   {
     "stevearc/conform.nvim",
     opts = function(_, opts)
-      if LazyVim.has_extra("formatting.prettier") then
-        opts.formatters_by_ft = opts.formatters_by_ft or {}
-        opts.formatters_by_ft.astro = { "prettier" }
-      end
+      opts.formatters_by_ft = opts.formatters_by_ft or {}
+      opts.formatters_by_ft.astro = { "prettier" }
     end,
   },
 }
