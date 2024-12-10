13384c22954425a8829f48da0172c090459cba9d
Karen Grigoryan <karen@karenjs.com>
2024-12-10 02:26:56 +0100

    chore: prune alfred workflows

--- home/Alfred.alfredpreferences/workflows/user.workflow.42BBCB33-550F-4149-B68E-D3C30F86027A/scripts/get-oldfiles.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-# WARN do not embed this file into the js file, otherwise it does retrieve the
-# proper oldfiles, since it then lacks information on the location of the shada
-# file for some reason
-temp=/tmp/oldfiles.txt
-[[ -e "$temp" ]] && rm "$temp"
-nvim -c "redir > $temp | echo v:oldfiles | redir end | q" &>/dev/null
-cat "$temp"
