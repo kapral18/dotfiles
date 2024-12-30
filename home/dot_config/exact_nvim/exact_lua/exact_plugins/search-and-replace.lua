a3baaf3fbd8ba02fab4259911c4a4a57d9dbe097
Karen Grigoryan <karen@karenjs.com>
2024-12-30 23:52:40 +0100

    feat: cleanup rust and brew packages
    * add `diskonaut`, `presenterm`, and `tokei` to Brewfile
    * remove `viu` from Brewfile
    * update `search-and-replace.lua` to use new keybindings for CtrlSF plugin
    * remove unused plugins from `search-and-replace.lua`
    * update default cargo crates to remove `ast-grep`, `bandwhich`, `diskonaut`, `presenterm`, `tokei`, `trippy`, and `viu`

--- home/dot_config/exact_nvim/exact_lua/exact_plugins/search-and-replace.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/search-and-replace.lua
@@ -1,16 +1,12 @@
 return {
   {
-    "MagicDuck/grug-far.nvim",
-    enabled = false,
-  },
-  {
     "dyng/ctrlsf.vim",
     lazy = false,
     keys = {
-      { "<leader>srr", "<Plug>CtrlSFCwordPath", desc = "S&R Word Under Cursor" },
-      { "<leader>srb", "<Plug>CtrlSFCCwordPath", desc = "S&R Word Under Cursor (With Boundaries)" },
-      { "<leader>srl", "<Plug>CtrlSFPwordPath", desc = "S&R Last Search Path" },
-      { "<leader>srr", "<Plug>CtrlSFVwordPath", desc = "S&R Word Under Cursor", mode = "x" },
+      { "<leader>sfw", "<Plug>CtrlSFCwordPath", desc = "S&R Word Under Cursor" },
+      { "<leader>sfb", "<Plug>CtrlSFCCwordPath", desc = "S&R Word Under Cursor (With Boundaries)" },
+      { "<leader>sfl", "<Plug>CtrlSFPwordPath", desc = "S&R Last Search Path" },
+      { "<leader>sfv", "<Plug>CtrlSFVwordPath", desc = "S&R Word Under Cursor", mode = "x" },
     },
     init = function()
       vim.g.ctrlsf_backend = "rg"
