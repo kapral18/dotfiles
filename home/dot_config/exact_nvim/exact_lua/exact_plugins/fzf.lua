b71b10f8abd923c9b6c77c5688cdbd67e2af37b4
Karen Grigoryan <karen@karenjs.com>
2025-10-28 01:26:46 +0100

    refactor: Reorder FZF Lua configuration for better organization - Move all `snacks` related keybindings into their own dedicated section. - This improves readability and maintainability of the FZF Lua plugin configuration.

--- home/dot_config/exact_nvim/exact_lua/exact_plugins/fzf.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/fzf.lua
@@ -41,53 +41,47 @@ return {
         end,
         desc = "Open Snipe buffer menu",
       },
-      { "<leader>,",       function() require("snacks").picker.buffers() end,                                                   desc = "Buffers", },
-      { "<leader>.",       function() require("snacks").scratch() end,                                                          desc = "Toggle Scratch Buffer", },
-      { "<leader><space>", get_fzf_fn("files", { cwd = vim.uv.cwd() }),                                                         desc = "Files" },
-      { "<leader>i",       get_fzf_fn("files", { fd_opts = fd_opts_unrestricted, cwd = vim.uv.cwd() }),                         desc = "Files" },
-      { "<leader>/",       get_fzf_fn("lgrep_curbuf"),                                                                          desc = "Grep" },
-      { "<leader>:",       "<cmd>Telescope command_history<cr>",                                                                desc = "Command History" },
-      { "<leader>sg",      function() require("fzf-lua").live_grep({ rg_opts = rg_opts, cwd = vim.uv.cwd() }) end,              desc = "Live Grep", },
-      { "<leader>sG",      function() require("fzf-lua").live_grep({ rg_opts = rg_opts_unrestricted, cwd = vim.uv.cwd() }) end, desc = "Live Grep (+ ignored)", },
       {
-        "<leader>fr",
-        function()
-          require("snacks").picker.recent({
-            filter = {
-              cwd = true,
-              paths = {
-                [vim.fn.stdpath("data")] = false,
-                [vim.fn.stdpath("cache")] = false,
-                [vim.fn.stdpath("state")] = false,
-                [vim.fn.getcwd() .. '.git/objects'] = false,
-                [vim.fn.getcwd() .. '.git/refs'] = false,
-                [vim.fn.getcwd() .. '.git/logs'] = false,
-                [vim.fn.getcwd() .. '.git/rr-cache'] = false
-              }
-            }
-          })
-        end,
-        desc = "Recent (cwd)"
+        "<leader><space>",
+        get_fzf_fn("files", { cwd = vim.uv.cwd() }),
+        desc = "Files",
       },
-      { "<leader>sR",  function() require("snacks").picker.resume() end,             desc = "Resume Picker List" },
-      { "<leader>sh",  function() require("snacks").picker.help() end,               desc = "Help Pages" },
-      { "<leader>sk",  function() require("snacks").picker.keymaps() end,            desc = "Key Maps" },
-      { "<leader>sc",  function() require("snacks").picker.commands() end,           desc = "Commands" },
-      { "<leader>sa",  function() require("snacks").picker.autocmds() end,           desc = "Auto Commands" },
-      { "<leader>sC",  function() require("snacks").picker.colorschemes() end,       desc = "Colorscheme with Preview" },
-      { "<leader>sm",  function() require("snacks").picker.marks() end,              desc = "Jump to Mark" },
-      { "<leader>sH",  function() require("snacks").picker.highlights() end,         desc = "Search Highlight Groups" },
-      { "<leader>s\"", function() require("snacks").picker.registers() end,          desc = "Registers", },
-      { "<leader>s/",  function() require("snacks").picker.search_history() end,     desc = "Search History", },
-      { "<leader>sB",  function() require("snacks").picker.grep_buffers() end,       desc = "Grep Open Buffers", },
-      { "<leader>sd",  function() require("snacks").picker.diagnostics() end,        desc = "Diagnostics", },
-      { "<leader>sD",  function() require("snacks").picker.diagnostics_buffer() end, desc = "Buffer Diagnostics", },
-      { "<leader>si",  function() require("snacks").picker.icons() end,              desc = "Icons", },
-      { "<leader>sj",  function() require("snacks").picker.jumps() end,              desc = "Jumps", },
-      { "<leader>sM",  function() require("snacks").picker.man() end,                desc = "Man Pages", },
-      { "<leader>sp",  function() require("snacks").picker.lazy() end,               desc = "Search for Plugin Spec", },
-      { "<leader>sq",  function() require("snacks").picker.qflist() end,             desc = "Quickfix List", },
-      { "<leader>sl",  function() require("snacks").picker.loclist() end,            desc = "Location List", },
+      {
+        "<leader>i",
+        get_fzf_fn("files", { fd_opts = fd_opts_unrestricted, cwd = vim.uv.cwd() }),
+        desc = "Files",
+      },
+      {
+        "<leader>/",
+        get_fzf_fn("lgrep_curbuf"),
+        desc = "Grep",
+      },
+      {
+        "<leader>:",
+        "<cmd>Telescope command_history<cr>",
+        desc = "Command History",
+      },
+      {
+        "<leader>sg",
+        function()
+          require("fzf-lua").live_grep({ rg_opts = rg_opts, cwd = vim.uv.cwd() })
+        end,
+        desc = "Live Grep",
+      },
+      {
+        "<leader>sG",
+        function()
+          require("fzf-lua").live_grep({ rg_opts = rg_opts_unrestricted, cwd = vim.uv.cwd() })
+        end,
+        desc = "Live Grep (+ ignored)",
+      },
+      {
+        "<leader>sR",
+        function()
+          require("fzf-lua").resume()
+        end,
+        desc = "Resume Picker List",
+      },
       {
         "<leader>sw",
         function()
@@ -95,8 +89,7 @@ return {
             search = vim.fn.expand("<cword>"),
             rg_opts = rg_opts,
             no_esc = true,
-            cwd =
-                vim.uv.cwd(),
+            cwd = vim.uv.cwd(),
           })
         end,
         desc = "Live Grep CWord",
@@ -106,8 +99,7 @@ return {
         function()
           require("fzf-lua").live_grep({
             search = vim.fn.expand("<cword>"),
-            rg_opts =
-                rg_opts_unrestricted,
+            rg_opts = rg_opts_unrestricted,
             no_esc = true,
             cwd = vim.uv.cwd(),
           })
@@ -118,8 +110,7 @@ return {
         "<leader>sw",
         function()
           require("fzf-lua").live_grep({
-            search = vim.trim(require("fzf-lua").utils
-              .get_visual_selection()),
+            search = vim.trim(require("fzf-lua").utils.get_visual_selection()),
             rg_opts = rg_opts .. " --multiline",
             no_esc = false,
             cwd = vim.uv.cwd(),
@@ -132,8 +123,7 @@ return {
         "<leader>sW",
         function()
           require("fzf-lua").live_grep({
-            search = vim.trim(require("fzf-lua").utils
-              .get_visual_selection()),
+            search = vim.trim(require("fzf-lua").utils.get_visual_selection()),
             rg_opts = rg_opts_unrestricted .. " --multiline",
             no_esc = false,
             cwd = vim.uv.cwd(),
@@ -141,6 +131,169 @@ return {
         end,
         mode = "v",
         desc = "Live Grep Selection (+ignored)",
+      },
+
+      -- Snacks
+
+      {
+        "<leader>,",
+        function()
+          require("snacks").picker.buffers()
+        end,
+        desc = "Buffers",
+      },
+      {
+        "<leader>.",
+        function()
+          require("snacks").scratch()
+        end,
+        desc = "Toggle Scratch Buffer",
+      },
+      {
+        "<leader>fr",
+        function()
+          require("snacks").picker.recent({
+            filter = {
+              cwd = true,
+              paths = {
+                [vim.fn.stdpath("data")] = false,
+                [vim.fn.stdpath("cache")] = false,
+                [vim.fn.stdpath("state")] = false,
+                [vim.fn.getcwd() .. ".git/objects"] = false,
+                [vim.fn.getcwd() .. ".git/refs"] = false,
+                [vim.fn.getcwd() .. ".git/logs"] = false,
+                [vim.fn.getcwd() .. ".git/rr-cache"] = false,
+              },
+            },
+          })
+        end,
+        desc = "Recent (cwd)",
+      },
+      {
+        "<leader>sh",
+        function()
+          require("snacks").picker.help()
+        end,
+        desc = "Help Pages",
+      },
+      {
+        "<leader>sk",
+        function()
+          require("snacks").picker.keymaps()
+        end,
+        desc = "Key Maps",
+      },
+      {
+        "<leader>sc",
+        function()
+          require("snacks").picker.commands()
+        end,
+        desc = "Commands",
+      },
+      {
+        "<leader>sa",
+        function()
+          require("snacks").picker.autocmds()
+        end,
+        desc = "Auto Commands",
+      },
+      {
+        "<leader>sC",
+        function()
+          require("snacks").picker.colorschemes()
+        end,
+        desc = "Colorscheme with Preview",
+      },
+      {
+        "<leader>sm",
+        function()
+          require("snacks").picker.marks()
+        end,
+        desc = "Jump to Mark",
+      },
+      {
+        "<leader>sH",
+        function()
+          require("snacks").picker.highlights()
+        end,
+        desc = "Search Highlight Groups",
+      },
+      {
+        '<leader>s"',
+        function()
+          require("snacks").picker.registers()
+        end,
+        desc = "Registers",
+      },
+      {
+        "<leader>s/",
+        function()
+          require("snacks").picker.search_history()
+        end,
+        desc = "Search History",
+      },
+      {
+        "<leader>sB",
+        function()
+          require("snacks").picker.grep_buffers()
+        end,
+        desc = "Grep Open Buffers",
+      },
+      {
+        "<leader>sd",
+        function()
+          require("snacks").picker.diagnostics()
+        end,
+        desc = "Diagnostics",
+      },
+      {
+        "<leader>sD",
+        function()
+          require("snacks").picker.diagnostics_buffer()
+        end,
+        desc = "Buffer Diagnostics",
+      },
+      {
+        "<leader>si",
+        function()
+          require("snacks").picker.icons()
+        end,
+        desc = "Icons",
+      },
+      {
+        "<leader>sj",
+        function()
+          require("snacks").picker.jumps()
+        end,
+        desc = "Jumps",
+      },
+      {
+        "<leader>sM",
+        function()
+          require("snacks").picker.man()
+        end,
+        desc = "Man Pages",
+      },
+      {
+        "<leader>sp",
+        function()
+          require("snacks").picker.lazy()
+        end,
+        desc = "Search for Plugin Spec",
+      },
+      {
+        "<leader>sq",
+        function()
+          require("snacks").picker.qflist()
+        end,
+        desc = "Quickfix List",
+      },
+      {
+        "<leader>sl",
+        function()
+          require("snacks").picker.loclist()
+        end,
+        desc = "Location List",
       },
     },
     opts = util.get_fzf_opts(),
