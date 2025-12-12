228fe1902c862bd3d07638f4c072e78431b92444
Karen Grigoryan <karen@karenjs.com>
2025-12-12 21:10:29 +0100

    refactor: Remove unused hammerspoon components and brew packages
    - Remove `RecursiveBinder` spoon from Hammerspoon configuration.
    - Delete `keymaps.lua` and `launch.lua` files.
    - Refactor `init.lua` to remove references to deleted components.
    - Delete `RecursiveBinder` from `.chezmoiexternal.toml`.
    - Remove `sox` for work environment.
    - Remove `asah` package from Brewfile.

--- home/dot_hammerspoon/init.lua
+++ home/dot_hammerspoon/init.lua
@@ -3,22 +3,8 @@ Hyper = { "ctrl", "alt", "cmd" }
 -- spoons
 pcall(function() hs.loadSpoon("EmmyLua") end)
 
-pcall(function()
-  hs.loadSpoon("RecursiveBinder")
-  if spoon.RecursiveBinder then
-    spoon.RecursiveBinder.helperFormat.textFont = "Avenir Next Condensed"
-    spoon.RecursiveBinder.helperFormat.atScreenEdge = 2
-    spoon.RecursiveBinder.helperEntryLengthInChar = 25
-  end
-end)
-
--- plugins
-local keymapsSetup = require("keymaps")
-require("launch")
 -- require("gridmouse")
 require("window")
-
-if keymapsSetup then keymapsSetup() end
 
 -- config reload
 hs.hotkey.bind(Hyper, "r", function()
